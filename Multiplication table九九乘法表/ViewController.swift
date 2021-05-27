f93d6e875c923c08d1aab2908c337f8ec408dfa2
Rose <qqbunny@icloud.com>
2021-05-27 16:42:23 +0800

    Initial Commit

---
+++ Multiplication table九九乘法表/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multiplication table九九乘法表
+//
+//  Created by Rose on 2021/5/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
